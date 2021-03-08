19be28ff474d4bf2bac2646f83e25c3968db3a7a
kalyak <kalyakusumadi@gmail.com>
2021-03-08 19:01:29 +0800

    Basic portfolio.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,11 @@ module.exports = {
   siteMetadata: {
     title: "Kalya Kusumadi Portfolio",
   },
-  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-offline"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-offline",
+  ],
 };
